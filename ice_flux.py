c36e7a715692408d0ce35ccdcfb294a6f3d88c4e
Polona Itkin <geopolonica@gmail.com>
2018-03-05 15:43:29 +0100

    Make intermediate changes.

--- ice_flux.py
+++ ice_flux.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 import numpy as np
-from pyhdf.SD import SD, SDC
 from netCDF4 import Dataset
 from glob import glob
+from pyresample import image, geometry
+import pyresample as pr
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
@@ -21,17 +22,20 @@ iclist = sorted(glob(icpath+'ice_conc_nh_polstere-100_multi_*.nc'))
 f = Dataset(iclist[0])
 lats = f.variables['lat'][:]
 lons = f.variables['lon'][:]
+xc = f.variables['xc'][:]
+yc = f.variables['yc'][:]
 f.close()
 
 #get the gate mask
+xx, yy = np.meshgrid(xc, yc)
 #north
-mask1 = (lats < 83.2) * (lats > 83) * (lons>5.) * (lons<70.)
+mask1 = (xx < 610) * (xx > 600) * (yy>-500) * (yy<300)
 #south
-mask2 = (lats < 79.2) * (lats > 79) * (lons>5.) * (lons<70.)
+mask2 = (xx < 1010) * (xx > 1000) * (yy>-500) * (yy<300)
 #east
-mask3 = (lats < 83.2) * (lats > 79) * (lons>70.) * (lons<70.7)
+mask3 = (xx < 1000) * (xx > 600) * (yy>300) * (yy<310)
 #west
-mask4 = (lats < 83.2) * (lats > 79) * (lons>5.) * (lons<5.7)
+mask4 = (xx < 1000) * (xx > 600) * (yy>-510) * (yy<-500)
 #combine  
 gates = mask1+mask2+mask3+mask4
 
@@ -42,6 +46,10 @@ coarse_lats = f.variables['lat'][:]
 coarse_lons = f.variables['lon'][:]
 f.close()
 
+#define both grids (for reprojection later on)
+area_def = pr.utils.load_area('area.cfg', 'osi_icecon')
+area_def_coarse = pr.utils.load_area('area.cfg', 'osi_drift')
+
 for fn in iclist:
     print fn
     date = fn.split('_')[-1].split('.')[0]
@@ -51,56 +59,42 @@ for fn in iclist:
     #if int(date)<20150120:continue
     #if int(date)>20150320:continue    
     
-    #reproject the sea ice drift to the ice concentration grid (62.5 > 10km)
+    #sea sea ice concentration data
     f = Dataset(fn)
-    icecon = f.variables['ice_conc'][:]
+    icecon = f.variables['ice_conc'][0,:,:]
     f.close()
     
+    #read sea ice displacement data
     f = Dataset(idpath+'ice_drift_nh_polstere-625_multi-oi_'+date+'-'+enddate+'.nc')
-    dx = f.variables['dX'][:]
-    dy = f.variables['dY'][:]
+    dx = f.variables['dX'][0,:,:]
+    dy = f.variables['dY'][0,:,:]
     f.close()
+    
+    #calculate drift
+    u_coarse = dx/(2*24*60*60)
+    v_coarse = dx/(2*24*60*60)
    
-    
-    
-    from pyresample import image, geometry
-    
-    print coarse_lats.shape
-    print lats.shape
-    exit()
+    #reproject the sea ice drift to the ice concentration grid (62.5 > 10km)
+    tmp = image.ImageContainerNearest(u_coarse, area_def_coarse, radius_of_influence=60)
+    u = tmp.resample(area_def).image_data
+    tmp = image.ImageContainerNearest(v_coarse, area_def_coarse, radius_of_influence=60)
+    v = tmp.resample(area_def).image_data
 
-#>>> area_def = geometry.AreaDefinition('areaD', 'Europe (3km, HRV, VTC)', 'areaD',
-#...                                {'a': '6378144.0', 'b': '6356759.0',
-#...                                 'lat_0': '50.00', 'lat_ts': '50.00',
-#...                                 'lon_0': '8.00', 'proj': 'stere'},
-#...                                800, 800,
-#...                                [-1370912.72, -909968.64,
-#...                                 1029087.28, 1490031.36])
-#>>> msg_area = geometry.AreaDefinition('msg_full', 'Full globe MSG image 0 degrees',
-#...                                'msg_full',
-#...                                {'a': '6378169.0', 'b': '6356584.0',
-#...                                 'h': '35785831.0', 'lon_0': '0',
-#...                                 'proj': 'geos'},
-#...                                3712, 3712,
-#...                                [-5568742.4, -5568742.4,
-#...                                 5568742.4, 5568742.4])
-#>>> data = np.ones((3712, 3712))
-#>>> msg_con_quick = image.ImageContainerQuick(data, msg_area)
-#>>> area_con_quick = msg_con_quick.resample(area_def)
-#>>> result_data_quick = area_con_quick.image_data
-#>>> msg_con_nn = image.ImageContainerNearest(data, msg_area, radius_of_influence=50000)
-#>>> area_con_nn = msg_con_nn.resample(area_def)
-#>>> result_data_nn = area_con_nn.image_data    
-    
-    
-    
-
-
-    #read sea ice drift maps
-    #make daily averages
+    #mask all the data
+    u_gates = np.ma.array(u,mask=~gates)
+    v_gates = np.ma.array(v,mask=~gates)
+    ic_gates = np.ma.array(icecon,mask=~gates)
     
     #calculate the area flux
+    #calculation has to be separate for all gates - sign will be opposite depending on the gate
+    flux_n = v_n * ic_n *100
+    flux_s = v_s * ic_s *100
+    flux_e = u_e * ic_e *100
+    flux_w = u_w * ic_w *100
+    
     #sum up for all gates
+    flux = -flux_n + flux_s - flux_e + flux_w
+    
     #store in time series
 
 
@@ -115,6 +109,9 @@ for fn in iclist:
                 projection='stere', lat_ts=82, lat_0=90., lon_0=0.,
                 llcrnrlon=0, llcrnrlat=79,
                 urcrnrlon=80, urcrnrlat=80)
+    
+     
+    
 
     m.drawmapboundary(fill_color='#9999FF')
     m.drawcoastlines()
@@ -126,10 +123,12 @@ for fn in iclist:
 
     #plot the ice concentration
     x, y = m(lons, lats)
-    im = m.pcolormesh(x, y, icecon, cmap=plt.cm.jet)
+    #im = m.pcolormesh(x, y, icecon, cmap=plt.cm.jet)
+    #m.colorbar()
     
     #plot the gates
-    im = m.pcolor(x, y, gates)
+    im = m.pcolor(x, y, ic_gates)
+    m.colorbar()
     
     fig.tight_layout()
     outname = 'icecon'+date
